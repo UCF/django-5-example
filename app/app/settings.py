a2af8c424b9ddaafd07f5dec76919fe83817a433
ucfchandra <chandra.kethireddy@ucf.edu>
2024-06-22 03:07:10 -0400

    working hello world

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-dpin4of)f1e5#+t%xh3_9&c$z%z)v$$@z^h8ur&ybf6n18gz--
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'pages',
 ]
 
 MIDDLEWARE = [
